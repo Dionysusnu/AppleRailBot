bb64c66879162ec9e38542d4df08aead10c27aca
Dionysusnu <dion.niessen@gmail.com>
2019-07-28 19:08:24 +0200

    fix bugs, delete reboot

--- commands/embed.js
+++ commands/embed.js
@@ -8,8 +8,8 @@ module.exports = {
 	cooldown: 1,
 	cannotDisable: false,
 	aliases: [''],
-	execute(message, args) {
-        // We can create embeds using the MessageEmbed constructor
+	async execute(message, args) {
+		// We can create embeds using the MessageEmbed constructor
 		// Read more about all that you can do with the constructor
 		// over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
 		const embed = new RichEmbed()
@@ -20,6 +20,6 @@ module.exports = {
 		// Set the main content of the embed
 			.setDescription('Hello, this is a slick embed!');
 		// Send the embed to the same channel as the message
-        message.channel.send(embed);
-    },
+		message.channel.send(embed);
+	},
 };
--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,15 @@
 module.exports = {
 	name: 'ping',
-    description: 'Replies with a simple message.\nYou can use it to check if the bot is working.',
-    guildOnly: false,
-    args: false,
-    cooldown: 5,
-    cannotDisable: false,
-	execute(message) {
-        const date = new Date();
-        message.channel.send('Pong!')
-            .then(msg => {
-                msg.edit('Pong! ' + (parseInt(msg.createdTimestamp) - parseInt(message.createdTimestamp)) + 'ms');
-            });
-        // or message.reply('pong') to add mention to message author
-    },
+	description: 'Replies with a simple message.\nYou can use it to check if the bot is working.',
+	guildOnly: false,
+	args: false,
+	cooldown: 5,
+	cannotDisable: false,
+	async execute(message) {
+		message.channel.send('Pong!')
+			.then(msg => {
+				msg.edit('Pong! ' + (parseInt(msg.createdTimestamp) - parseInt(message.createdTimestamp)) + 'ms');
+			});
+		// or message.reply('pong') to add mention to message author
+	},
 };
