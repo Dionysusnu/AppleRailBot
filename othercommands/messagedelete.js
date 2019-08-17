2426c73022c7d091952974c0772cbe06a185037e
Dionysusnu <dion.niessen@gmail.com>
2019-08-17 11:07:20 +0200

    revert to module.exports

--- othercommands/messagedelete.js
+++ othercommands/messagedelete.js
@@ -1,20 +1,22 @@
 const { RichEmbed } = require('discord.js');
 
-export const name = 'messagedeleted';
-export const description = 'Can assign roles to people if they reply to a message with an emoji';
-export const guildOnly = false;
-export const args = false;
-export const cooldown = 0;
-export const aliases = [''];
-export async function execute(message) {
-	const logs = await message.guild.fetchAuditLogs({ type: 'MESSAGE_DELETE' });
-	const filtered = logs.entries.filter(log => log.target === message.author && log.extra.channel === message.channel).first();
-	const embed = new RichEmbed();
-	embed.setTitle((filtered && filtered.avatarURL || message.author.avatarURL) + (filtered && filtered.tag || message.author.tag));
-	embed.setDescription('**Message deleted**');
-	embed.addField('Author', `${message.author}`);
-	embed.addField('Channel', `${message.channel}`);
-	message.content && embed.addField('Content', message.content);
-	embed.attachFiles(message.attachments.array());
-	message.guild.channels.get(process.env.DISCORD_LOGS_ID).send('', embed);
+module.exports = {
+	name: 'messagedeleted',
+	description: 'Can assign roles to people if they reply to a message with an emoji',
+	guildOnly: false,
+	args: false,
+	cooldown: 0,
+	aliases: [''],
+	async execute(message) {
+		const logs = await message.guild.fetchAuditLogs({ type: 'MESSAGE_DELETE' });
+		const filtered = logs.entries.filter(log => log.target === message.author && log.extra.channel === message.channel).first();
+		const embed = new RichEmbed();
+		embed.setTitle((filtered && filtered.avatarURL || message.author.avatarURL) + (filtered && filtered.tag || message.author.tag));
+		embed.setDescription('**Message deleted**');
+		embed.addField('Author', `${message.author}`);
+		embed.addField('Channel', `${message.channel}`);
+		message.content && embed.addField('Content', message.content);
+		embed.attachFiles(message.attachments.array());
+		message.guild.channels.get(process.env.DISCORD_LOGS_ID).send('', embed);
+	}
 }
