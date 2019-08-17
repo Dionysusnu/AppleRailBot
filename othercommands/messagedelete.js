3b14be3b217a0fcb5366b974d46d3e419326997b
Dionysusnu <dion.niessen@gmail.com>
2019-08-17 17:12:04 +0200

    remove avatar url, add timestamp

--- othercommands/messagedelete.js
+++ othercommands/messagedelete.js
@@ -13,12 +13,13 @@ module.exports = {
 		const filtered = logs.entries.filter(log => log.target === message.author && log.extra.channel === message.channel).first();
 		console.log(filtered);
 		const embed = new RichEmbed();
-		embed.setTitle((filtered && filtered.executor.avatarURL || message.author.avatarURL) + ' ' + (filtered && filtered.executor.tag || message.author.tag));
+		embed.setTitle(filtered && filtered.executor.tag || message.author.tag);
 		embed.setDescription('**Message deleted**');
 		embed.addField('Author', `${message.author}`);
 		embed.addField('Channel', `${message.channel}`);
 		message.content && embed.addField('Content', message.content);
 		embed.attachFiles(message.attachments.array());
+		embed.setTimestamp();
 		message.guild.channels.get(process.env.DISCORD_LOGS_ID).send('', embed);
 	},
 };
