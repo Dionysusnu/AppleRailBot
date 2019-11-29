c4e351d4aa0563283bdc23b71948d4eba0031b53
Dionysusnu <dion.niessen@gmail.com>
2019-11-29 18:42:43 +0100

    add tagrole.js

---
+++ commands/tagrole.js
@@ -0,0 +1,23 @@
+const roles = new Map([
+	['td', 500318304070795275],
+	['qd', 500318174076600321],
+	['pss', 466942558325571606],
+	['lts', 419817477170790400],
+	['dev', 599283987348979712],
+	['shift', 599283872810663956],
+]);
+module.exports = {
+	name: 'tagrole',
+	description: '',
+	guildOnly: true,
+	args: true,
+	cooldown: 5,
+	cannotDisable: false,
+	async execute(message, args) {
+		const role = message.guild.roles.get(roles.get(args[0].toLowerCase()));
+		const mentionable = role.mentionable;
+		mentionable || await role.setMentionable(true, 'tag');
+		await message.channel.send(`^^ ${role}`);
+		await role.setMentionable(mentionable, 'after tag');
+	},
+};
